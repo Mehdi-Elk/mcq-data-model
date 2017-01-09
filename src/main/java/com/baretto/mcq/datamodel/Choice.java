047407a01b66003bfcf01db931f0a7e74ef66029
Mehdi-Elk <m.elketroussi@gmail.com>
2017-01-09 10:44:18 +0100

    initial import

---
+++ src/main/java/com/baretto/mcq/datamodel/Choice.java
@@ -0,0 +1,24 @@
+package com.baretto.mcq.datamodel;
+
+/**
+ * Represent one choice for a unique MCQ question.
+ *
+ * Created by mehdi on 05/01/17.
+ */
+public interface Choice {
+
+    /**
+     * Get the choice's label which be visible for a user.
+     *
+     * @return the label
+     */
+    String getLabel();
+
+    /**
+     * Check if the choice is valid for the MCQ question.
+     *
+     * @return true if the choice is valid ,
+     *          false in other case.
+     */
+    boolean isValid();
+}
---
+++ src/main/java/com/baretto/mcq/datamodel/internals/ChoiceImpl.java
@@ -0,0 +1,52 @@
+package com.baretto.mcq.datamodel.internals;
+
+import com.baretto.mcq.datamodel.Choice;
+
+import java.io.Serializable;
+
+/**
+ * Simple implementation of Choice for MCQ.
+ * Created by mehdi on 07/01/17.
+ */
+public final class ChoiceImpl implements Choice, Serializable {
+
+    private final String label;
+    private final boolean isValid;
+
+    ChoiceImpl(String aLabel, boolean isValid) {
+        this.label = aLabel;
+        this.isValid = isValid;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ChoiceImpl choice = (ChoiceImpl) o;
+
+        if (isValid != choice.isValid) {
+            return false;
+        }
+        return label.equals(choice.label);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = label.hashCode();
+        result = 31 * result + (isValid ? 1 : 0);
+        return result;
+    }
+}
