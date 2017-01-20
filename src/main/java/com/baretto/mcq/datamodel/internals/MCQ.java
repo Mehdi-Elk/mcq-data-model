a7dc2828cefef936b768d86556ad568513a2ee7f
djakpat <djakce2@hotmail.fr>
2017-01-20 11:20:16 +0100

    Mock de questions

--- src/main/java/com/baretto/mcq/datamodel/internals/MCQ.java
+++ src/main/java/com/baretto/mcq/datamodel/internals/MCQ.java
@@ -7,10 +7,11 @@ import com.baretto.mcq.datamodel.Question;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * A MCQ (multiple choice questions) contain all the question for a test.
- *
+ * <p>
  * Created by WORK on 14/01/2017.
  */
 public class MCQ implements Serializable {
@@ -20,33 +21,41 @@ public class MCQ implements Serializable {
 
     /**
      * generate a MCQ with "i" questions
+     *
      * @param i
      */
     public MCQ(int i) {
 
-        questions =  generateQuestion(i);
-        
+        questions = generateQuestion(i);
+
     }
 
     /**
      * Temporary method for generate question.
+     *
      * @param i
      * @return
      */
     private Set<Question> generateQuestion(int i) {
         Set<Question> theQuestions = new HashSet<Question>();
-        for (int j = 0; j <= i; j++) {
-            Choice firstChoice = new ChoiceImpl("firstLabel"+j);
-            Choice secondChoice = new ChoiceImpl("SecondLabel"+j);
+        for (int j = 0; j < i; j++) {
+            Choice firstChoice = new ChoiceImpl(j + ".1 : " + "The Product Owner cannot accurately report progress to the stakeholders");
+            Choice secondChoice = new ChoiceImpl(j + ".2 : " + "Impediments are raised and resolved more slowly");
+            Choice thirdChoice = new ChoiceImpl(j + ".3 : " + "The Scrum Master loses the ability to update the Gantt chart properly");
+            Choice fourthChoice = new ChoiceImpl(j + ".4 : " + "Oppprtunities to inspect and adapt the Sprint Backlog are lost");
+            Choice fithChoice = new ChoiceImpl(j + ".5 : " + "The Sprint plan becomes inaccurate ");
+            Choice sixthChoice = new ChoiceImpl(j + ".6 : " + "Too much work is spent updating the Scrum board before the meeting ");
 
-            Set <Choice> choices = new HashSet<Choice>();
+
+            Set<Choice> choices = new TreeSet<Choice>();
             choices.add(firstChoice);
             choices.add(secondChoice);
 
-            Set <Choice> correctChoices = new HashSet<Choice>();
+            Set<Choice> correctChoices = new HashSet<Choice>();
             correctChoices.add(firstChoice);
 
-           Question question = new QuestionImpl("label"+j,choices,correctChoices, AnswerConstraint.ONE_RESPONSE);
+            final String questionLabel = "The Daily Scrum is an event that happens every day. What would be three key concerns if the frequency were to be lowered to every two or three days?";
+            Question question = new QuestionImpl(" j : " + questionLabel, choices, correctChoices, AnswerConstraint.ALL_THAT_APPLY);
             theQuestions.add(question);
         }
         return theQuestions;
@@ -55,6 +64,7 @@ public class MCQ implements Serializable {
 
     /**
      * get all the questions of the MCQ
+     *
      * @return
      */
     public Set<Question> getQuestions() {
